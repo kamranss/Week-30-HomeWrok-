5e8a2d73be966ac3915d577c3f0edaee0756e47b
kamranss <kamranss@code.edu.az>
2023-03-13 20:34:29 +0400

    Refactor: task updated

---
+++ assets/script/basketHomePage.js
@@ -0,0 +1,52 @@
+let buttons = document.querySelectorAll(".btn-primary");
+
+
+buttons.forEach(btn =>{
+    btn.addEventListener("click", function(e){
+        e.preventDefault();
+        let id = btn.parentNode.getAttribute("data-id");
+        imgSRC = btn.parentNode.previousElementSibling.getAttribute("src");
+        productName = btn.parentNode.firstElementChild.innerText
+        price = btn.nextElementSibling.innerText
+
+        console.log(id);
+
+        if (console.log(localStorage.getItem("basket")==! null)) {
+            getArr = JSON.parse(localStorage.getItem("basket"))
+
+            getArr.push({
+                id: id,
+                imgSRC: imgSRC,
+                productName: productName,
+                price:price,
+                count: 1
+            })
+
+            localStorage.setItem("basket", JSON.stringify(getArr));
+            console.log(getArr);
+        }
+        else{
+
+            let arr =[];
+        
+        arr.push({
+            id: id,
+            imgSRC: imgSRC,
+            productName: productName,
+            price:price,
+            count: 1
+        })
+        
+
+        
+        console.log(imgSRC);
+        console.log(productName);
+        console.log(price);
+        console.log(arr);
+
+        localStorage.setItem("basket", JSON.stringify(arr))
+        }
+        
+        
+    })
+})
